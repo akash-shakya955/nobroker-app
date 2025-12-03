bca2f3386b78e9cd083f97411a7d141e0293b4af
akash-shakya955 <mr.akashshakya@gmail.com>
2025-12-03 11:50:14 +0530

    Initial commit: Full NoBroker backend with user auth

---
+++ README.md
@@ -0,0 +1,2 @@
+# nobroker-app
+
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Routes
+import userRoutes from "./src/routes/userRoutes.js";
+app.use("/api/users", userRoutes);
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("NoBroker backend running!");
+});
+
+// MongoDB connect
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
